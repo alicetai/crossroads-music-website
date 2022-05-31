dd6596ff6d6130d6cd63d885db7e7e347fb38563
alicetai <58418239+alicetai@users.noreply.github.com>
2022-05-31 11:29:24 +1000

    Designed adding to cart confirmation modal

---
+++ scripts/cart.js
@@ -0,0 +1,28 @@
+// Cart functions
+
+// function alertShow() {
+//     document.getElementById("item-added-confirmation").style.display = "block";
+// }
+
+// function alertHide() {
+//     document.getElementById("item-added-confirmation").style.display = "none";
+// }
+
+// const cartAdd = document.getElementById('add-to-cart-btn');
+
+// cartAdd.addEventListener('click', () => {
+//     setTimeout(() => {
+//         const confirmation = document.getElementById('item-added-confirmation');
+//         confirmation.style.display = 'block';
+//     // box.style.visibility = 'hidden';
+//     }, 1000);
+// });
+
+// $(document).ready(function(){
+//     $(".btn1").click(function(){
+//         $("p").hide();
+//     });
+//     $(".btn2").click(function(){
+//         $("p").show();
+//     });
+// });
