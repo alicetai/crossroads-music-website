1daf1efa3370c528745bfdc39e5843c1ab03c8f6
alicetai <58418239+alicetai@users.noreply.github.com>
2022-06-01 23:55:41 +1000

    Got stepper to display correctly

--- scripts/stepper.js
+++ scripts/stepper.js
@@ -1,8 +1,34 @@
+let currentStep = 1;
+
 window.onload = function() {
-    showStep(0); // Display the first form step
+    showStep(currentStep); // Display the first form step
 };
 
 function showStep(n) {
+    // Display the elements of the form
     var steps = document.getElementsByClassName("form-step");
     steps[n].style.display = "flex";
+
+    // Display the active step on the stepper
+    var stepperNumbers = document.getElementsByClassName("step-number");
+    stepperNumbers[n].style.backgroundColor = "#493722"
+    var stepperLabels = document.getElementsByClassName("step-label");
+    stepperLabels[n].style.color = "#493722"
+}
+
+// Display the next step of the form
+function toggleStep(n) {
+    var steps = document.getElementsByClassName("form-step");
+    // if (n == 1 && !validateForm()) return false;
+    steps[currentStep].style.display = "none";
+    currentStep = n;
+    // if (current >= steps.length) {
+    //     document.getElementById("regForm").submit();
+    //     return false;
+    // }
+    showStep(currentStep);
+}
+
+function validateForm() {
+    return true;
 }
